77c98dc9135a8e6dd9b71fff73463a7e417cacac
Lucas Wojciechowski <lucas@lucaswoj.com>
2014-07-25 16:49:44 -0400

    Updated reduced size problem

--- project/code/loadReducedProblem.m
+++ project/code/loadReducedProblem.m
@@ -1,8 +1,9 @@
 function loadReducedProblem()
 
-    global nTasks tasks nRobots robots nHomes homes priorities skills distances;
+    global nTasks tasks nRobots robots nHomes homes velocities priorities skills distances;
 
     % TODO remove unnescessary globals (tasks, robots)
+    % TODO turn on those assert statements
 
     nTasks = 3;
     tasks = 1:nTasks;
@@ -10,10 +11,17 @@ function loadReducedProblem()
     nRobots = 2;
     robots = 1:nRobots;
 
+    velocities = [1, 2];
+
+    times = [
+        1, 4, 5, 0, 0;
+        2, 3, 6, 0, 0
+    ];
+
     nHomes = nRobots;
     homes = (1:nRobots) + nTasks;
 
-    priorities =  [0.1, 0.2, 0.3, 1, 1];
+    priorities = [0.1, 0.2, 0.3, 1, 1];
     % assert(size(priorities) == [nTasks])
 
     skills = [
@@ -23,10 +31,10 @@ function loadReducedProblem()
     % assert(size(priorities) == [nTasks, nRobots])
 
     distances =  [
-        0,   0.1, 0.2, 0.6, 0.5;
-        0.1, 0,   0.3, 0.3, 0.4;
-        0.2, 0.3, 0,   0.1, 0.2;
-        0.6, 0.3, 0.1, 0,   0;
-        0.5, 0.4, 0.2, 0,   0
+        0, 1, 2, 6, 5;
+        1, 0, 3, 3, 4;
+        2, 3, 0, 1, 2;
+        6, 3, 1, 0, 0;
+        5, 4, 2, 0, 0
     ];
     % assert(size(priorities) == [nTasks + nHomes, nRobots + nHomes])
