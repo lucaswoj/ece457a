1844f496fd66e2387c1d725707d8a1ada5287f7e
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-07-26 15:43:54 -0400

    Fixed a typo. "interta" -> "inertia"

--- project/code/pso/getVelocity.m
+++ project/code/pso/getVelocity.m
@@ -1,6 +1,6 @@
 function velocity = getVelocity(position, v0, lbest, gbest, w, r1, r2, c1, c2)
 
-  inertaVelocity = multiplyScalarVelocity(w, v0)
+  inertiaVelocity = multiplyScalarVelocity(w, v0)
   lbestVelocity = c1 * r1 * subtractPositions(lbest, position)
   gbestVelocity = c2 * r2 * subtractPositions(gbest, position)
 
