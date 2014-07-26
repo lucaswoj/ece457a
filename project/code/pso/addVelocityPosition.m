cd4b8c3a2dfee35686e64c4fe53de639699eb399
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-07-26 14:57:29 -0400

    refactored a line to be more concise.

--- project/code/pso/addVelocityPosition.m
+++ project/code/pso/addVelocityPosition.m
@@ -2,6 +2,6 @@ function position = addVelocityPosition(velocity, position)
   assert(size(velocity, 1) == 2)
 
   for i = 1:length(velocity)
-    swap = [velocity(i, 1), velocity(i, 2)]
+    swap = velocity(i)
     position(swap) = position(fliplr(swap))
   endfor
