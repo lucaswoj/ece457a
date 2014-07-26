21c95979be134a027d5af2402f0c2cefb8a364b8
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-07-26 15:42:57 -0400

    Implemented the multiplication of a velocity by a scalar in permutation PSO.

--- project/code/pso/multiplyScalarVelocity.m
+++ project/code/pso/multiplyScalarVelocity.m
@@ -1,1 +1,14 @@
 function product = multiplyScalarVelocity(scalar, velocity)
+	numSwaps = size(velocity, 1);
+	newNumSwaps = scalar * numSwaps;
+
+	if scalar < 1
+		product = velocity(1:newNumSwaps, :) % Remove numModification entries from velocity
+	elseif scalar > 1
+		product = zeros(newNumSwaps, 2);
+		for i = 0:newNumSwaps-1
+			product(i+1, :) = velocity(mod(i, numSwaps)+1, :);
+		endfor
+	else
+		product = velocity;
+	end
