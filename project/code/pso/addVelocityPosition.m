b447639f32d375e202c1b9e73928f98a4f3f0f65
ARIEL WEINGARTEN <ASWEINGA@uwaterloo.ca>
2014-07-26 15:52:30 -0400

    Corrected implementation of addVelocityPosition

--- project/code/pso/addVelocityPosition.m
+++ project/code/pso/addVelocityPosition.m
@@ -1,7 +1,7 @@
 function position = addVelocityPosition(velocity, position)
-  assert(size(velocity, 1) == 2)
+  assert(size(velocity, 2) == 2)
 
-  for i = 1:length(velocity)
-    swap = velocity(i)
+  for i = 1:size(velocity,1)
+    swap = velocity(i, :)
     position(swap) = position(fliplr(swap))
   endfor
