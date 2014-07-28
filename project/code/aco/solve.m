45d8f9116aebbc9614ad55169f205fa6f284a838
Sandy Maguire <sandy@sandymaguire.me>
2014-07-28 11:48:33 -0400

    Fixed ACO

--- project/code/aco/solve.m
+++ project/code/aco/solve.m
@@ -1,35 +1,39 @@
 function bestSolution = solve(iterations)
     % initializeProblem();
 
-    global homes;
+    global homes energy;
 
     % construct the pheromone list
     numNodes = homes(end) - 1;
     numTasks = homes(1) - 1;
     tau = ones(numNodes + 1);
 
-    alpha = 1;
-    beta = 1;
+    exploitConst = 1;
+    exploreConst = 1;
     rho = 0.1;
     rnaught = 0.5;
     Q = 1; % pheromone amount
 
     bestSolution = [ ];
-    bestDist = Inf;
+    mostDesired = -Inf;
 
+    totalEnergy = energy
     ants = cell(2,3)
 
     for i = 1 : iterations
         for ant = 1:length(ants)
+            energy = totalEnergy
+
             robot = 1;
             currentNode = homes(robot);
             visited = zeros(1, numNodes);
 
             path = [ ];
-            distTraveled = 0;
+            desirability = 0;
             while sum(visited == 0) ~= 0
                 neighbors = [ ];
                 neighborWeight = [ ];
+                neighborTauWeight = [ ];
                 for n = 1 : length(visited)
                     if visited(n) == 0
                         if n > numTasks
@@ -43,37 +47,35 @@ function bestSolution = solve(iterations)
                     end
                 end
 
-                % Generate random number r, and compare to pre-chose r_o.
-                % If r <= r_o then exploit most desirable path
-                % Else explore path randomly selected using roulette wheel method
-
                 % compute neighbor weights
+                neighborWeight = 1 : length(neighbors)
+                neighborTauWeight = 1 : length(neighbors)
                 for j = 1 : length(neighbors)
                     n = neighbors(j);
 
-                    % add 1 to the denominator to avoid division by 0
-                    neighborWeight = [ neighborWeight, (weight(robot, currentNode, n, path)^beta + 1) ];
+                    neighborWeight(j) = weight(robot, currentNode, n)^exploreConst + 1;
+                    neighborTauWeight(j) = neighborWeight(j) * tau(currentNode, n);
                 end
 
+                % Generate random number r, and compare to pre-chose r_o.
+                % If r <= r_o then exploit most desirable path
+                % Else explore path randomly selected using roulette wheel method
                 if rand <= rnaught
-                    [maxWeight,selectedNeighbor] = max(neighborWeight); % getindex of most desirable neighbor
+                    [maxWeight, selectedNeighbor] = max(neighborTauWeight);
                     nextNode = neighbors(selectedNeighbor);
                 else
-
                     eta = 0;
                     for j = 1 : length(neighbors)
                         n = neighbors(j);
-                        eta = eta + tau(currentNode, n)^alpha * neighborWeight(j);
+                        eta = eta + tau(currentNode, n)^exploitConst * neighborWeight(j);
                     end
 
                     eta
 
-                    cumlProb = [ ];
+                    cumlProb = 1 : length(neighbors);
                     for j = 1 : length(neighbors)
                         n = neighbors(j);
-
-                        prob = tau(currentNode, n)^alpha * neighborWeight(j) / eta;
-                        cumlProb = [ cumlProb, prob ];
+                        cumlProb(j) = tau(currentNode, n)^exploitConst * neighborWeight(j) / eta;
                     end
 
                     cumlProb
@@ -81,10 +83,13 @@ function bestSolution = solve(iterations)
                     nextNode = neighbors(selectedNeighbor)
                 end
 
-                distTraveled = distTraveled + neighborWeight(selectedNeighbor);
                 visited(nextNode) = 1
                 path = [ path, nextNode ]
+                desirability = desirability + neighborWeight(selectedNeighbor);
+                energy(robot) = energy(robot) - energyCost(robot, currentNode, nextNode)
+
                 if nextNode == homes(robot)
+                    % teleport to a new robot if we've returned home
                     robot = robot + 1;
 
                     try
@@ -97,10 +102,12 @@ function bestSolution = solve(iterations)
                 currentNode = nextNode;
             end
 
-            if distTraveled < bestDist
-                % transform depot nodes to sentinels
-                bestSolution = path .* (path <= numTasks);
-                bestDist = distTraveled;
+            % get a canonical representation of the solution
+            solution = path .* (path <= numTasks);
+
+            if desirability > mostDesired
+                bestSolution = solution; 
+                mostDesired = desirability;
             end
 
             augmentedPath = [ ]
@@ -111,32 +118,33 @@ function bestSolution = solve(iterations)
                 end
             end
             path = [ homes(1), augmentedPath, homes(end) ];
-            ants{ant, 1} = bestSolution
-            ants{ant, 2} = bestDist
-            ants{ant, 3} = augmentedPath
+
+            ants{ant, 1} = getSolutionCost(solution)
+            ants{ant, 2} = desirability
+            ants{ant, 3} = path
         end
+
         % determine ant with best solution:
-        solutionCosts = 1:length(ants);
-        for ant = 1:length(ants)
-            solution = ants{ant, 1}
-            solutionCosts(ant) = getSolutionCost(solution);
+        solutionCosts = 1 : length(ants);
+        for ant = 1 : length(ants)
+            solutionCosts(ant) = ants{ant, 1};
         end
+
         [cost, selectedAnt] = max(solutionCosts);
-        path = ants{selectedAnt, 1}
-        desirability = ants{selectedAnt, 2}
-        augmentedPath = ants{selectedAnt, 3}
+        desirability = ants{selectedAnt, 2};
+        path = ants{selectedAnt, 3};
 
         % update the pheromone
         tau = tau .* (1 - rho);
-        for j = 1 : length(augmentedPath) - 1
-            j
-            tau(augmentedPath(j), augmentedPath(j + 1)) = tau(augmentedPath(j), augmentedPath(j + 1)) + Q * desirability;
-            tau(augmentedPath(j + 1), augmentedPath(j)) = tau(augmentedPath(j + 1), augmentedPath(j)) + Q * desirability;
+        for j = 1 : length(path) - 1
+            tau(path(j), path(j + 1)) = tau(path(j), path(j + 1)) + Q * desirability;
+            tau(path(j + 1), path(j)) = tau(path(j + 1), path(j)) + Q * desirability;
         end
 
         desirability
         tau
     end
 
-    bestDist
+    mostDesired
 end
+
--- project/code/aco/weight.m
+++ project/code/aco/weight.m
@@ -1,50 +1,27 @@
-function v = weight(robot, src, dst, curPath)
+function v = weight(robot, src, dst)
     global homes priorities skills taskTimes distances velocities;
 
-    % alpha = 1;
-    % beta = 1;
-    % gamma = 1;
-
-    % v = distance(src, dst);
-    % if dst == homes(robot)
-    %     return;
-    % end
-
-    % v = v * (alpha - priority(dst)) ^ gamma;
-    % v = v * (beta - skill(robot, dst));
-
-    % determine which case we're in
-
-    if dst == homes(robot)
-        v = 0.5
-    elseif canMakeItHomeFrom(dst, curPath, robot, dst)
-        % figure out better way to import alpha and beta
+    v = 0.0000000001;
+    if canMakeItHomeFrom(robot, src, dst)
+        % figure out better way to import alpha
         alpha = 0.5;
-        v = (1-alpha)*(1-priorities(dst))*skills(robot,dst);
-        v = v + getBeta()*alpha*( (distances(src,dst))/velocities(robot) + taskTimes(robot, dst) )
-    else
-        v = 0.0000000001
-        % v = -Inf
+        v = (1 - alpha) * (1 - priorities(dst)) * skills(robot, dst);
+        v = v + getBeta() * alpha * energyCost(robot, src, dst);
     end
 
 end
 
-function isTrue = canMakeItHomeFrom(node, curPath, robot, dst)
-    global homes energy taskTimes distances velocities;
+function isTrue = canMakeItHomeFrom(robot, src, dst)
+    global homes energy;
 
     energyLeft = energy(robot);
-    curPath = [curPath, dst];
+    energyLeft = energyLeft - energyCost(robot, src, dst);
 
-    % need to determine energy expended so far
-    for i = length(curPath):-1:2
-        if curPath(i) >= homes(1) % if we encounter ANY home node, then stop
-            break
-        else
-            energyLeft = energyLeft - (taskTimes(i) + distances(curPath(i-1), curPath(i)));
-        end
+    if dst ~= homes(robot)
+        % we can make it home from home, obvi
+        energyLeft = energyLeft - energyCost(robot, dst, homes(robot));
     end
 
-    % return true if there will be enough energy to make it home from dst
-    isTrue = (energyLeft >= ((distances(dst, homes(robot)))/velocities(robot)))
+    isTrue = energyLeft >= 0;
 end
 
